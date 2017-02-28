70a9aa5aa5e73a34e3f4577785db6bec34bd7d8e
Felipe Jaramillo Gómez <pipeluck@gmail.com>
2017-02-27 22:17:21 -0500

    Add Maeterial

--- app/angular-application/src/app/app.component.ts
+++ app/angular-application/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Employee } from './shared/employee.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,23 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  persons = [{ name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-2.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-3.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-4.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-5.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-6.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-7.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-8.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-9.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-10.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-11.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-12.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-13.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-14.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-15.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-16.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-17.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-18.jpg' },
-  { name: 'Felipe', imageUrl: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-19.jpg' }]
-  title = 'app works!';
+  employees = [new Employee('Felipe Jaramillo Gómez',
+                            'Developer',
+                            'none'),
+                            new Employee('Juan David Herrera',
+                            'Developer',
+                            'none'),
+                            new Employee('Pablo Díaz',
+                            'Developer',
+                            'none'),
+                            new Employee('Carlos Ángulo',
+                            'Developer',
+                            'none')];
+
 }
---
+++ app/angular-application/src/app/shared/employee.model.ts
@@ -0,0 +1,7 @@
+export class Employee {
+    constructor(public name: string,
+        public position: string,
+        public profileUrl: string) {
+
+    }
+}
